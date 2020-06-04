7ebd6e8c78a83551bcc72cabda940e4712a04d18
Ankit Gautam <ankit.gautam@flipkart.com>
2020-06-04 12:22:20 +0530

    Initial dist subtree commit

---
+++ precache-manifest.d7186b77379fccf7263152ce8fb52f88.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8939a6c044d7c1f53616db47b006962c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b97fdd86b66e0a9040d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8b39d4556a6df0b9a4c4",
+    "url": "/static/css/main.09a7b35f.chunk.css"
+  },
+  {
+    "revision": "8b97fdd86b66e0a9040d",
+    "url": "/static/js/2.98606807.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.98606807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b39d4556a6df0b9a4c4",
+    "url": "/static/js/main.8d34b633.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "0af35e78000d271b765fe5eb60dec48a",
+    "url": "/static/media/cloudy-day-1.0af35e78.svg"
+  },
+  {
+    "revision": "7fd0e682f007856fae9042ff54a83776",
+    "url": "/static/media/cloudy-day-3.7fd0e682.svg"
+  },
+  {
+    "revision": "9f4d576225cc4504a1397e4ed6cf180b",
+    "url": "/static/media/cloudy.9f4d5762.svg"
+  },
+  {
+    "revision": "539254f220b7ba35e1fc0497c6ade293",
+    "url": "/static/media/day.539254f2.svg"
+  },
+  {
+    "revision": "544c7ef40abca7366cc313543a059a5e",
+    "url": "/static/media/rainy-1.544c7ef4.svg"
+  },
+  {
+    "revision": "c13b9ec13c339530bfaf34fd1fed8b0a",
+    "url": "/static/media/rainy-6.c13b9ec1.svg"
+  },
+  {
+    "revision": "c2d53a747a3b7fa1626ebc90e3b620d3",
+    "url": "/static/media/snowy-1.c2d53a74.svg"
+  },
+  {
+    "revision": "d30350d8ff3d9e1b63c37367bc84413b",
+    "url": "/static/media/thunder.d30350d8.svg"
+  }
+]);
